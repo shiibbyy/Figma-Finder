b3a45774f46ef2db08455f121a471bb439e83baf
Kieran Parker <kieran7_7@hotmail.com>
2018-10-04 17:00:56 +0100

    Initial commit

---
+++ src/components/home/Hero.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="section hero">
+        <div class="container">
+            <div class="content">
+                <h1 class="light">Figma Finder</h1>
+                <h3 class="light">All the Figma Resources you need. </br>
+                Right here, right now, found by us.</h3>
+                <div class="form-row">
+                    <input type="text" placeholder="Enter Email">
+                    <button class="btn"><p class="semibold light">Sign Up!</p></button>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Hero'
+}
+</script>
+
+<style lang="scss">
+@import '@/styles/global.scss';
+
+.hero {
+    height: 70vh;
+    background-color: $off-black;
+}
+
+h1 {
+    padding-bottom: 20px;
+}
+
+.form-row {
+    display: flex;
+    padding-top: 80px;
+}
+
+.btn {
+    margin-left: 10px;
+}
+
+.hero .content {
+    margin-top: -30px;
+}
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="FigmaFinder"/> -->
+    <Hero />
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import Hero from '@/components/home/Hero.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld,
+    Hero
+  }
+}
+</script>
